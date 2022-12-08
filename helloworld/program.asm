4541730a589c8c4e8ca335eb22618b4135b69b8e
battlecatsahead <48861210+battlecatsahead@users.noreply.github.com>
2022-12-08 16:06:24 -0500

    hello world

---
+++ helloworld/program.asm
@@ -0,0 +1,21 @@
+; x86 assembly program template for Linux
+
+; initialize data segment
+segment .data
+    msg db 'hello'          ; define msg as byte, and set its value to 'hello'
+    SYS_write equ 1         ; define SYS_write as constant and set its value to 1
+    msg_len equ $ - msg     ; define msg_len as constant and set its value to length of msg
+
+; initialize code segment
+segment .text
+    global _start
+
+; _start function - entry point for the program
+_start:
+    mov rdi, 1          ; specify file descriptor for standard output
+    lea rsi, [msg]      ; load address of msg into ecx
+    mov rdx, msg_len    ; specify length of data to be written
+    mov rax, SYS_write  ; specify syscall number for write
+    syscall             
+    mov eax, 1
+    syscall
