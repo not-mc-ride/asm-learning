51695667745525b58d5c97c8c9dda631d0667058
battlecatsahead <48861210+battlecatsahead@users.noreply.github.com>
2022-12-14 11:44:20 -0500

    add newline function

--- 04.function/main.asm
+++ 04.function/main.asm
@@ -1,10 +1,10 @@
-
 ; initialize data segment
 segment .data
     msg: db "hello world"
     msg_len equ $ - msg
     msg2: db "ello world"
     msg2_len equ $ - msg2
+    nline: db 10
     exit_code equ 60
     exit_call equ 1
     SYS_write equ 1         ; define SYS_write as constant and set its value to 1
@@ -22,6 +22,7 @@ _start:
     push msg               ; send msg to the top of the stack
     push msg_len
     call print
+    call newline
     push msg2
     push msg2_len
     call print             ; call print function after pudhing both the message and the message length
@@ -31,6 +32,14 @@ exit:                      ; create exit function modtly for cleaner code
     mov rsi, exit_call
     syscall  
 
+newline:
+    mov rax, 1
+    mov rsi, nline
+    mov rdi, 1
+    mov rdx, 1
+    syscall
+    ret
+
 print:                      ; define a print function 
     pop rax                 ; pop the return address to rax
     mov [retadr], rax       ; move the return address to a variable, since i use rax later in the function     
@@ -39,6 +48,7 @@ print:                      ; define a print function
     pop rsi 
     mov rax, SYS_write
     syscall
+    mov rdi,1
     mov rax, [retadr]
     push rax                ; push rax to the top of the stack, since thats what ret uses to know where to return
     ret                     ; return to main loop
